dc47b31bb36fd773d536e69b47746dfd6de4018f
Norbiros <norbiros@protonmail.com>
2023-03-12 18:33:45 +0100

    Real init!

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosInstance, AxiosResponse } from "axios";
-import  { wrapper } from "axios-cookiejar-support"
-import { CookieJar } from "tough-cookie"
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
 
 export class Librus {
   private readonly cookie: CookieJar;
@@ -8,41 +8,34 @@ export class Librus {
 
   constructor() {
     this.cookie = new CookieJar();
-    this.cookie.setCookie("TestCookie=1;", "https://synergia.librus.pl")
+    this.cookie.setCookie('TestCookie=1;', 'https://synergia.librus.pl');
     this.caller = wrapper(
       axios.create({
         jar: this.cookie,
         withCredentials: true,
         headers: {
-          "User-Agent":
-            "User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36",
+          'User-Agent':
+            'User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36',
         },
-      })
+      }),
     );
   }
 
   authorize(login: string, pass: string): Promise<boolean> {
-    let caller = this.caller;
+    const caller = this.caller;
     return caller
-      .get(
-        "https://api.librus.pl/OAuth/Authorization?client_id=46&response_type=code&scope=mydata"
-      )
+      .get('https://api.librus.pl/OAuth/Authorization?client_id=46&response_type=code&scope=mydata')
       .then(() => {
-        return caller.postForm(
-          "https://api.librus.pl/OAuth/Authorization?client_id=46",
-          {
-            action: "login",
-            login: login,
-            pass: pass,
-          }
-        );
+        return caller.postForm('https://api.librus.pl/OAuth/Authorization?client_id=46', {
+          action: 'login',
+          login,
+          pass,
+        });
       })
       .then(() => {
-        return caller
-          .get("https://api.librus.pl/OAuth/Authorization/2FA?client_id=46")
-          .then(() => {
-            return true;
-          });
+        return caller.get('https://api.librus.pl/OAuth/Authorization/2FA?client_id=46').then(() => {
+          return true;
+        });
       })
       .catch((e: Promise<boolean>) => {
         console.error(e);
@@ -52,13 +45,13 @@ export class Librus {
 
   getUsers() {
     return this.caller
-      .get("https://synergia.librus.pl/gateway/api/2.0/Users")
+      .get('https://synergia.librus.pl/gateway/api/2.0/Users')
       .then((el: AxiosResponse<any>) => {
         return el.data.Users;
       })
       .catch((e) => {
         console.error(e);
-        return []
+        return [];
       });
   }
 }
